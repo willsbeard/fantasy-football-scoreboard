fde3abf1319cf5ed34a498aafadc36ffb2d142b1
Will Ayres <ayres.will@gmail.com>
2021-09-23 21:04:34 -0400

    league info

--- data/yahoo_api_parser.py
+++ data/yahoo_api_parser.py
@@ -87,27 +87,6 @@ def get_league(self, game_id, league_id, team_id, week):
         league_info = {}
         league_info['user_name'] = "test"
         return league_info
-#        for m in matchup:
-#            if not isinstance(matchup[m], int):
-#                team = matchup[m]['matchup']['0']['teams']
-#                print(team)
-#                for t in team:
-#                    if not isinstance(team[t], int):
-#                        if team[t]['team'][0][3]:
-#                            matchup_info['user_name'] = team[t]['team'][0][19]['managers'][0]['manager']['nickname']
-#                            matchup_info['user_av'] = team[t]['team'][0][19]['managers'][0]['manager']['nickname']
-#                            matchup_info['user_av_location'] = team[t]['team'][0][5]['team_logos'][0]['team_logo']['url']
-#                            matchup_info['user_team'] = team[t]['team'][0][2]['name']
-#                            matchup_info['user_proj'] = team[t]['team'][1]['team_projected_points']['total']
-#                            matchup_info['user_score'] = float(team[t]['team'][1]['team_points']['total'])
-#                        else:
-#                            matchup_info['opp_name'] = team[t]['team'][0][19]['managers'][0]['manager']['nickname']
-#                            matchup_info['opp_av'] = team[t]['team'][0][19]['managers'][0]['manager']['nickname']
-#                            matchup_info['opp_av_location'] = team[t]['team'][0][5]['team_logos'][0]['team_logo']['url']
-#                            matchup_info['opp_team'] = team[t]['team'][0][2]['name']
-#                            matchup_info['opp_proj'] = team[t]['team'][1]['team_projected_points']['total']
-#                            matchup_info['opp_score'] = float(team[t]['team'][1]['team_points']['total'])
-#        return league_info
 
     def get_avatars(self, teams):
         self.refresh_access_token()
