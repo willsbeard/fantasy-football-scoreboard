720d49e4c25c30253d3e11d05a6bdf2648b0d807
Will Ayres <ayres.will@gmail.com>
2021-10-01 15:17:09 -0400

    1

--- data/yahoo_api_parser.py
+++ data/yahoo_api_parser.py
@@ -104,8 +104,8 @@ class YahooFantasyInfo():
                 #print(standings[s]['team'][0][2]['name'])
                 #print(standings[s]['team'][2]['team_standings']['rank'])
                 #print(" ")
-                team_name = str(standings[s]['team'][0][2]['name'])
-                team_rank = str(standings[s]['team'][2]['team_standings']['rank'])
+                team_name = standings[s]['team'][0][2]['name']
+                team_rank = standings[s]['team'][2]['team_standings']['rank']
                 standing_info['team',s] = [team_name,team_rank]
                 #print(team_name," ", team_rank)
                 #standing_info['team',s] = [standings[s]['team'][0][2]['name'], standings[s]['team'][2]['team_standings']['rank']]
