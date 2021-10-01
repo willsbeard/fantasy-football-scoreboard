a4af54bb197d3df67f1da51a4ea2d716b146eb3f
Will Ayres <ayres.will@gmail.com>
2021-10-01 12:24:56 -0400

    1

--- data/yahoo_api_parser.py
+++ data/yahoo_api_parser.py
@@ -101,7 +101,7 @@ class YahooFantasyInfo():
             #print("S in STANDING")
             #print(s)
             if not isinstance(standings[s], int):
-                print(standings[s]['team'][0]['name'])
+                print(standings[s]['team'][0][2]['name'])
 
     def get_avatars(self, teams):
         self.refresh_access_token()
