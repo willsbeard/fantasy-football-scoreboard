6eac26c3da67aa62595d5f94e55bf682494bea40
Will Ayres <ayres.will@gmail.com>
2021-10-01 15:48:35 -0400

    1

--- data/yahoo_api_parser.py
+++ data/yahoo_api_parser.py
@@ -104,12 +104,13 @@ class YahooFantasyInfo():
                 #print(standings[s]['team'][0][2]['name'])
                 #print(standings[s]['team'][2]['team_standings']['rank'])
                 #print(" ")
-                team_name = standings[s]['team'][0][2]['name']
-                team_rank = standings[s]['team'][2]['team_standings']['rank']
-                standing_info['team',s] = [team_name,team_rank]
+                team_name = str(standings[s]['team'][0][2]['name'])
+                print(team_name)
+                #team_rank = standings[s]['team'][2]['team_standings']['rank']
+                #standing_info['team',s] = [team_name,team_rank]
                 #print(team_name," ", team_rank)
                 #standing_info['team',s] = [standings[s]['team'][0][2]['name'], standings[s]['team'][2]['team_standings']['rank']]
-        print(standing_info)
+        #print(standing_info)
 
     def get_avatars(self, teams):
         self.refresh_access_token()
