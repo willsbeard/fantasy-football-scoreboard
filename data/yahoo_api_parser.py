427e6c362994d9cf413dd0de0f1b09bb52635eb7
Will Ayres <ayres.will@gmail.com>
2021-10-01 16:26:24 -0400

    1

--- data/yahoo_api_parser.py
+++ data/yahoo_api_parser.py
@@ -115,8 +115,7 @@ class YahooFantasyInfo():
         for t in standing_info:
             rank = standing_info[t][0]
             final_standings_info[rank]=standing_info[t]
-        for q in final_standings_info:
-            print(final_standings_info[t][0])
+            print(final_standings_info[rank])
 
     def get_avatars(self, teams):
         self.refresh_access_token()
