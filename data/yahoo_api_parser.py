557be392e760849d47f759ea2a3cc80847d06df3
Will Ayres <ayres.will@gmail.com>
2021-10-01 15:56:09 -0400

    1

--- data/yahoo_api_parser.py
+++ data/yahoo_api_parser.py
@@ -101,12 +101,10 @@ class YahooFantasyInfo():
             #print(s)
             if not isinstance(standings[s], int):
                 #print("TEAM: ",standings[s]['team'][0][2]['name']," RANK: ",standings[s]['team'][2]['team_standings']['rank'])
-                #print(standings[s]['team'][0][2]['name'])
+                print(standings[s]['team'][0][2]['name'])
                 #print(standings[s]['team'][2]['team_standings']['rank'])
                 #print(" ")
-                team_name = standings[s]['team'][0][2]['name']
-                team_name2 = str(team_name)
-                print("TEAM: ",team_name2)
+                #team_name = standings[s]['team'][0][2]['name']
                 #team_rank = standings[s]['team'][2]['team_standings']['rank']
                 #standing_info['team',s] = [team_name,team_rank]
                 #print(team_name," ", team_rank)
