b758cdc9ff3c110377f2606a425294b2c0c44acb
Will Ayres <ayres.will@gmail.com>
2021-10-06 15:12:30 -0400

    1

--- renderer/main.py
+++ renderer/main.py
@@ -53,16 +53,12 @@ class MainRenderer:
         
         ##### MY CODE #####
         #self._draw_standings()
-        self.sleepEvent = sleepEvent
-        self.sleepEvent.clear()
         self.canvas.Clear()
         image = self._draw_standings()
 
         i = 0
 
-        self.sleepEvent.wait(5)
-
-        while i > -(67 - self.matrix.height) and not self.sleepEvent.is_set():
+        while i > -(67 - self.matrix.height):
                 i -= 1
 
                 self.canvas.Clear()
@@ -71,7 +67,7 @@ class MainRenderer:
                 #self.matrix.draw_image((25,0), gradient, align="center")
                 self.matrix.draw_image_layout(self.layout.info,image,(0, i))
 
-                self.sleepEvent.wait(0.3)
+                t.sleep(0.3)
 
 #        if self.week == 0 or (time.weekday() == 3 and time.hour >= 13):
 #            debug.info('Scheduled State, waiting 15 min')
