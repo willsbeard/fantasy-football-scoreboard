52427549a5fd906c2067953a0aafb2c6bbbb3120
Will Ayres <ayres.will@gmail.com>
2021-10-07 13:26:47 -0400

    1

--- renderer/main.py
+++ renderer/main.py
@@ -226,6 +226,10 @@ class MainRenderer:
         self.draw.rectangle([0, 27, 36, 21], fill=(206, 17, 38))
         self.draw.text((1, 21), "LAST GAME:", fill=(255, 255, 255),font=self.font_mini)
         self.draw.text((0, 28), "160-142".format(), fill=(255, 255, 255),font=self.font_mini)
+
+        self.draw.rectangle([0, 27, 36, 21], fill=(206, 17, 38))
+        self.draw.text((1, 42), "NEXT GAME:", fill=(255, 255, 255),font=self.font_mini)
+        self.draw.text((0, 49), "ME vs YOU".format(), fill=(255, 255, 255),font=self.font_mini)
         
         #self._refresh_image()
         #t.sleep(86400)
