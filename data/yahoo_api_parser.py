d95264d2f6c051071f78feaa6942fb30bfc6def7
Will Ayres <ayres.will@gmail.com>
2021-10-08 16:38:00 -0400

    3

--- data/yahoo_api_parser.py
+++ data/yahoo_api_parser.py
@@ -87,10 +87,10 @@ class YahooFantasyInfo():
                             matchup_info['opp_score'] = float(team[t]['team'][1]['team_points']['total'])
         return matchup_info
 
-    def get_all_matchups(self, game_id, league_id, team_id, week):
+    def get_all_matchups(self, game_id, league_id, week):
         self.refresh_access_token()
         #url = "https://fantasysports.yahooapis.com/fantasy/v2/team/{0}.l.{1}.t.{2}/matchups;weeks={3}".format(self.game_id, self.league_id, self.team_id, week)
-        url = "https://fantasysports.yahooapis.com/fantasy/v2/league/{0}.l.{1};out=scoreboard;week={3}?format=json".format(self.game_id, self.league_id, self.team_id, week)
+        url = "https://fantasysports.yahooapis.com/fantasy/v2/league/{0}.l.{1};out=scoreboard;week={2}?format=json".format(self.game_id, self.league_id, week)
         response = self.oauth.session.get(url, params={'format': 'json'})
         #print("URL")
         #print(url)
