707dc96da880d48c705e65e59715f8bcd2f20e56
Will Ayres <ayres.will@gmail.com>
2021-10-07 22:54:33 -0400

    1

--- renderer/main.py
+++ renderer/main.py
@@ -245,7 +245,7 @@ class MainRenderer:
         for s in league:
             team_name = league[s][1]
             rank = league[s][0]
-            self.draw.text((1, row_start), team_name, fill=(255, 255, 255),font=self.font_vs)
+            self.draw.text((1, row_start), team_name, fill=(255, 255, 255),font=self.font)
             row_start = row_start + 7
 
         t.sleep(5)
