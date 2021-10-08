a529dc818e798175e813c3fe999ef788a9904cf1
Will Ayres <ayres.will@gmail.com>
2021-10-07 22:44:59 -0400

    1

--- renderer/main.py
+++ renderer/main.py
@@ -240,11 +240,12 @@ class MainRenderer:
         self.draw.text((1, 0), "RANKS:".format(), fill=(255,255,255),font=self.font_mini)
         #self.draw.text((1, 7), "5-3-1".format(), fill=(255, 255, 255),font=self.font_mini)
         row_start = 7
-        self.draw.text((1, 7), team_name, fill=(255, 255, 255),font=self.font_mini)
-        #for t in league:
-        #    team_name = league[t][1]
-        #    self.draw.text((1, row_start), team_name, fill=(255, 255, 255),font=self.font_mini)
-        #    row_start = row_start + 7
+        #team_name = league[0][1]
+        #self.draw.text((1, 7), team_name, fill=(255, 255, 255),font=self.font_mini)
+        for s in league:
+            team_name = league[s][1]
+            self.draw.text((1, row_start), team_name, fill=(255, 255, 255),font=self.font_mini)
+            row_start = row_start + 7
 
         t.sleep(10)
 
